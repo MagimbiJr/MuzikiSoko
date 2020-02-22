69348f8d5b20f3d08425fa12894b6f42fcde2c98
magimbijr <charleatanasi@gmail.com>
2020-02-22 19:51:25 +0300

    Wrapping icons inside Buttons in Artist Portal

--- src/components/Artist Profile/Discography/AlbumFull/albumFull.js
+++ src/components/Artist Profile/Discography/AlbumFull/albumFull.js
@@ -5,16 +5,16 @@ import '../../Discography/discography.css'
 export default () => (
     <>
       <div className="album_details">
-      <div className="btn_paragraph">
+        <div className="btn_paragraph">
           <button className="toggle_down_btn">
             <i className="fas fa-caret-down"></i>
           </button>
           <p>Lingala Ya Yesu</p>
-          </div>
-          <div className="flex_separator_one"></div>
-          <p className="price_label">Ksh.500</p>
-          <button className="album_action">Buy Album</button>
-          <button className="album_action disabled" disabled>Download</button>
+        </div>
+        <div className="flex_separator_one"></div>
+        <p className="price_label">Ksh.500</p>
+        <button className="album_action">Buy Album</button>
+        <button className="album_action disabled" disabled>Download</button>
       </div>
       <div className="song_details  ">
         <div className="btn_paragraph">
